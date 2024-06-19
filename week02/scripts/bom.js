619bbc6c5306f0d6fc96360a47254319643b4c2e
georgiecalamity <brylledannt@gmail.com>
2024-06-19 20:26:34 +0800

    bom top 10

---
+++ week02/scripts/bom.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+function handleClick() {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement("button");
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    input.value = '';
+
+    deleteButton.addEventListener('click', function() {
+      list.removeChild(li);
+      input.focus();
+    });
+
+  } else {
+    input.focus();
+  }
+}
+
+button.addEventListener('click', handleClick);
+
