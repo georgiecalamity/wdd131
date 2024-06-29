4e252eb95b039f4853a23a101213e5fb97c67921
georgiecalamity <brylledannt@gmail.com>
2024-06-29 19:19:46 +0800

    added image

--- scripts/place.js
+++ scripts/place.js
@@ -11,7 +11,7 @@ function calculateWindChill(temperature, windSpeed) {
     if (temperature < 10 || windSpeed > 4.8) {
       return "N/A";
     }
-  
+
     const tempF = (temperature * 9/5) + 32;
     const windChill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(windSpeed, 0.16)) 
                       + (0.4275 * tempF * Math.pow(windSpeed, 0.16));
@@ -21,6 +21,6 @@ function calculateWindChill(temperature, windSpeed) {
   }
   
 const temperature = 34;
-const windSpeed = 24; 
+const windSpeed = 2; 
 const result = calculateWindChill(temperature, windSpeed);
 document.getElementById('windchill').textContent = ` ${result}`;
